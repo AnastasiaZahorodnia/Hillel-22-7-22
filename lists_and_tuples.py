c9175ef2e654604f96d50a550098d5aa970f4fda
AnastasiaZahorodnia <anastasiazahorodnia@gmail.com>
2022-08-19 21:27:34 +0200

    home work 6

---
+++ lists_and_tuples.py
@@ -0,0 +1,24 @@
+new_str = input("Tell me something:").split()
+print(new_str[2::3])
+# djn = el for el in new_str if new_str
+
+incoming_list = [1, 2.1, "f", "2", 3, "1", 18, "df"]
+outgoing_list = []
+for point_element in incoming_list:
+
+    if isinstance(point_element, float):
+        outgoing_list.append(point_element)
+
+    elif isinstance(point_element, int) and point_element % 2 == 0:
+        outgoing_list.append(point_element)
+
+    elif isinstance(point_element, int) and point_element % 2 != 0:
+        outgoing_list.append(point_element ** 2)
+
+    elif isinstance(point_element, str) and point_element.isdigit():
+        outgoing_list.append(int(point_element) * 3)
+
+    elif isinstance(point_element, str):
+        outgoing_list.append(-1)
+
+print(outgoing_list)
