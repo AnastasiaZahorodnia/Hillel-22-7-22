3e6ac19c61b7b593c3e668161e7ee442df7a125a
AnastasiaZahorodnia <anastasiazahorodnia@gmail.com>
2022-08-25 16:55:56 +0200

    Lesson 6

--- lists_and_tuples.py
+++ lists_and_tuples.py
@@ -1,25 +1,31 @@
 new_str = input("Tell me something:").split()
 print(new_str[2::3])
-# = el for el in new_str if new_str
 
 incoming_list = [1, 2.1, "f", "2", 3, "1", 18, "df"]
-outgoing_list = []
 
-for point_element in incoming_list:
+gen_list = [x if type(x) == float else
+            x if type(x) == int and x % 2 == 0 else
+            x **2 if type(x) == int and x % 2 != 0 else
+            int(x)*3 if type(x) == str and x.isdigit()
+            else -1 for x in incoming_list]
+print(gen_list)
 
-    if isinstance(point_element, float):
-        outgoing_list.append(point_element)
+#outgoing_list = []
+#for point_element in incoming_list:
 
-    elif isinstance(point_element, int) and point_element % 2 == 0:
-        outgoing_list.append(point_element)
+#    if type(point_element) == float:
+#        outgoing_list.append(point_element)
 
-    elif isinstance(point_element, int) and point_element % 2 != 0:
-        outgoing_list.append(point_element ** 2)
+#    elif type(point_element) == int and point_element % 2 == 0:
+#        outgoing_list.append(point_element)
 
-    elif isinstance(point_element, str) and point_element.isdigit():
-        outgoing_list.append(int(point_element) * 3)
+#    elif type(point_element) == int and point_element % 2 != 0:
+#        outgoing_list.append(point_element ** 2)
 
-    else:
-        outgoing_list.append(-1)
+#    elif type(point_element) == str and point_element.isdigit():
+#        outgoing_list.append(int(point_element) * 3)
 
-print(outgoing_list)
+#    else:
+#        outgoing_list.append(-1)
+
+#print(outgoing_list)
